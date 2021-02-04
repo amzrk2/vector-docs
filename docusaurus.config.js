b6867878712eeb9edd1adb4f6f8468ff668d101c
amzrk2 <amzrk2@outlook.com>
2021-02-04 21:31:27 +0800

    init: docusaurus proj

---
+++ docusaurus.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  title: 'Vector Analytics',
+  tagline: 'Minimal alternative to Google Analytics',
+  url: 'https://appvector.icu',
+  baseUrl: '/',
+  favicon: 'favicon.ico',
+
+  organizationName: 'amzrk2',
+  projectName: 'vector-docs',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  themeConfig: {
+    navbar: {
+      title: 'My Site',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        { to: 'blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © 2020-${new Date().getFullYear()} DSRKafuU Built with Docusaurus`,
+    },
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
