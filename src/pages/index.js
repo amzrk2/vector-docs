f1419e4333d45887a62de89e5e1603ce70d69fdf
amzrk2 <amzrk2@outlook.com>
2021-02-05 12:52:13 +0800

    update: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,46 +9,27 @@ import VectorIcon from '../../static/vector.svg';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Minimal Analytics',
     description: (
-      <p>
-        Docusaurus was designed from the ground up to be easily installed and used to get your
-        website up and running quickly.
-      </p>
+      <p>Focuses exclusively on page views, user location/device and JavaScript events.</p>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'GDPR/CCPA Compliant',
     description: (
-      <p>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your
-        docs into the <code>docs</code> directory.
-      </p>
+      <p>No cookie usage, no cross-site tracking, collecting data completely anonymously.</p>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Serverless Support',
     description: (
-      <p>
-        Extend or customize your website layout by reusing React. Docusaurus can be extended while
-        reusing the same header and footer.
-      </p>
+      <p>No need to manually manage servers thanks to Serverless and cloud database services.</p>
     ),
   },
 ];
-
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
+function Feature({ title, description }) {
   return (
     <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
